f489ed036d3590dc7e7a2ced838580bd9ed1db5a
DONGBUM, KIM <dongbum9@gmail.com>
2019-08-05 15:58:36 +0900

    add test code.

--- stravafollowsync/controller/test.py
+++ stravafollowsync/controller/test.py
@@ -1,8 +1,31 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import render_template, request, url_for
 from stravafollowsync.stravafollowsync_blueprint import stravafollowsync
+from stravalib import Client
 
 @stravafollowsync.route('/test')
 def test():
-    return render_template('test.html')
+    client = Client()
+    url = client.authorization_url(client_id=MY_STRAVA_CLIENT_ID,
+                                   redirect_uri=url_for('.logged_in', _external=True),
+                                   approval_prompt='auto')
+
+    return render_template('test.html', authorize_url=url)
+
+@stravafollowsync.route('/strava_oauth')
+def logged_in():
+    error = request.args.get('error')
+    state = request.args.get('state')
+
+    if error:
+        return render_template('login_error.html', error=error)
+    else:
+        code = request.args.get('code')
+        client = Client()
+        access_token = client.exchange_code_for_token(client_id=MY_STRAVA_CLIENT_ID,
+                                                      client_secret=MY_STRAVA_CLIENT_SECRET,
+                                                      code=code)
+        strava_athlete = client.get_athlete()
+
+    return render_template('login_results.html', athlete=strava_athlete, access_token=access_token)
